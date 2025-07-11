3dec9316de09dbe7b9233745fe0b6ddb1daba20c
EdmundWagner-JL <edmundwagner98@gmail.com>
2025-07-11 18:56:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 import pgzrun
 
 HEIGHT=600
@@ -34,7 +34,7 @@ def draw():
 
 def display_message(heading,subheading):
     screen.draw.text(heading, fontsize=60, center=(400,300), color="black")
-    screen.draw.text(subheading, fontsize=30, center=(330,300), color="blue")
+    screen.draw.text(subheading, fontsize=30, center=(400,330), color="blue")
 
 def update():
     global items
@@ -44,9 +44,71 @@ def update():
 #Make items
 #1. select random items from ITEMS list
 #2. create actors and add it to items list
-#3. do the items with correct positions
-#4. move the objects down with animation
+#3. do the items with correct positions - layout items
+#4. move the objects down with animation - animate items
 
+def make_items(number_of_extra_items):
+    items_to_create = get_option_to_create(number_of_extra_items)
+    new_items = create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
 
+def get_option_to_create(number_of_extra_items):
+    items_to_create = ["paper"]
+    for i in range(0, number_of_extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
 
+def create_items(items_to_create):
+    new_items = []
+    for option in items_to_create:
+        item = Actor(option + "img")
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout) + 1
+    gaps_size = WIDTH / number_of_gaps
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos = (index + 1) * gaps_size
+        item.x= new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = start_speed-current_level
+        animation = animate(item, duration = duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over=True
+
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global items, current_level, animations, game_complete
+    stop_animation(animations)
+    if current_level == final_level:
+        game_complete = True
+    else:
+        current_level = current_level+1
+        animations=[]
+        items=[]
+
+def stop_animation(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+    
 pgzrun.go()
